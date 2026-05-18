9801fd065a8c5a79610b9cb87f76fced452060a3
Rxfael-0 <rafael.paiva.costa@escola.pr.gov.br>
2026-05-18 17:24:16 -0300

    Implement shop command to display available items
    Added a shop dictionary and a command to display items.

--- shop.py
+++ shop.py
@@ -1,1 +1,17 @@
+SHOP = {
+    "boost": 4,
+    "protecao": 3,
+    "maldição": 2
+}
 
+def setup_shop(bot):
+
+    @bot.command()
+    async def loja(ctx):
+
+        text = "🏪 LOJA\n\n"
+
+        for k, v in SHOP.items():
+            text += f"{k} - {v} 🪙\n"
+
+        await ctx.send(text)
