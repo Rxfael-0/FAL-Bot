9683a3d8ad4c501cb0f5ae3600ffc622b13d164b
Rxfael-0 <rafael.paiva.costa@escola.pr.gov.br>
2026-05-18 17:25:27 -0300

    Add Hall of Fame command to bot

--- hall.py
+++ hall.py
@@ -1,1 +1,24 @@
+import json
 
+FILE = "database/hall.json"
+
+def setup_hall(bot):
+
+    @bot.command()
+    async def halldafama(ctx, member: discord.Member, *, text: str):
+
+        try:
+            with open(FILE, "r") as f:
+                data = json.load(f)
+        except:
+            data = []
+
+        data.append({
+            "user": member.id,
+            "feito": text
+        })
+
+        with open(FILE, "w") as f:
+            json.dump(data, f, indent=4)
+
+        await ctx.send("🏆 registrado no Hall da Fama")
