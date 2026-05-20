ccf0daf12a202bf34911fb1db1cc82620f72a6c0
Rxfael-0 <rafael.paiva.costa@escola.pr.gov.br>
2026-05-19 21:30:13 -0300

    Refactor imports and update intents for bot

--- main.py
+++ main.py
@@ -2,28 +2,30 @@ import discord
 from discord.ext import commands
 import os
 
-from ranked import setup_ranked
-from queue import setup_queue
-from economy import setup_economy
-from shop import setup_shop
-from hall import setup_hall
-from clans import setup_clans
+from bot.ranked import setup_ranked
+from bot.hall import setup_hall
+from bot.shop import setup_shop
+from bot.queue import setup_queue
+from bot.clans import setup_clans
 
-intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
+intents = discord.Intents.all()
 
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-setup_ranked(bot)
-setup_queue(bot)
-setup_economy(bot)
-setup_shop(bot)
-setup_hall(bot)
-setup_clans(bot)
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
 
 @bot.event
 async def on_ready():
-    print(f"Bot online como {bot.user}")
+
+    print(
+        f"🔥 Bot ligado como {bot.user}"
+    )
+
+setup_ranked(bot)
+setup_hall(bot)
+setup_shop(bot)
+setup_queue(bot)
+setup_clans(bot)
 
 bot.run(os.getenv("TOKEN"))
