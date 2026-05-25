d9e25825bd4c88ee59cabfc24a7d5bb2eaec4472
Rxfael-0 <rafael.paiva.costa@escola.pr.gov.br>
2026-05-25 19:00:38 -0300

    Create sqlite.py

---
+++ sqlite.py
@@ -0,0 +1,58 @@
+import sqlite3
+
+conn = sqlite3.connect(
+    "database/database.db"
+)
+
+cursor = conn.cursor()
+
+# PLAYERS
+cursor.execute("""
+
+CREATE TABLE IF NOT EXISTS players (
+
+    user_id INTEGER PRIMARY KEY,
+
+    trofeus INTEGER DEFAULT 0,
+    medalhas INTEGER DEFAULT 0,
+    coins INTEGER DEFAULT 0,
+
+    wins INTEGER DEFAULT 0,
+    losses INTEGER DEFAULT 0,
+
+    shop_week INTEGER DEFAULT 0
+
+)
+
+""")
+
+# CLANS
+cursor.execute("""
+
+CREATE TABLE IF NOT EXISTS clans (
+
+    name TEXT PRIMARY KEY,
+
+    leader TEXT,
+    coleader TEXT,
+
+    wins INTEGER DEFAULT 0,
+    losses INTEGER DEFAULT 0,
+    surrenders INTEGER DEFAULT 0,
+
+    status TEXT,
+    last_activity TEXT,
+
+    role_id INTEGER,
+    channel_id INTEGER,
+
+    panel_channel INTEGER,
+    panel_message INTEGER,
+
+    logo TEXT
+
+)
+
+""")
+
+conn.commit()
