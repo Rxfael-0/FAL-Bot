fce34c62e9f406e50515966d07c77452d73917e1
Rxfael-0 <rafael.paiva.costa@escola.pr.gov.br>
2026-05-25 20:37:26 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,80 @@
+import discord
+from discord.ext import commands
+import os
+
+from ranked import setup_ranked
+from hall import setup_hall
+from shop import setup_shop, reset_shop_limits
+from economy import setup_economy
+from queue_system import setup_queue
+from clans import setup_clans, check_inactive
+from embed import setup_embed
+from tournament import setup_tournament
+
+# =========================
+# INTENTS
+# =========================
+
+intents = discord.Intents.default()
+
+intents.message_content = True
+intents.members = True
+
+# =========================
+# BOT
+# =========================
+
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
+
+# =========================
+# READY
+# =========================
+
+@bot.event
+async def on_ready():
+
+    print(f"🔥 Bot ligado como {bot.user}")
+
+    if not check_inactive.is_running():
+        check_inactive.start()
+
+    if not reset_shop_limits.is_running():
+        reset_shop_limits.start()
+
+# =========================
+# MESSAGE LOG
+# =========================
+
+@bot.event
+async def on_message(message):
+
+    if message.author.bot:
+        return
+
+    print("MSG:", message.content)
+
+    await bot.process_commands(message)
+
+# =========================
+# SYSTEMS
+# =========================
+
+setup_ranked(bot)
+setup_hall(bot)
+setup_shop(bot)
+setup_economy(bot)
+setup_queue(bot)
+setup_clans(bot)
+setup_embed(bot)
+setup_tournament(bot)
+
+# =========================
+# RUN
+# =========================
+
+bot.run(
+    os.getenv("TOKEN")
+)
