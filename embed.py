376154e16b9e0ff0d699204c23b5757b8fc520d5
Rxfael-0 <rafael.paiva.costa@escola.pr.gov.br>
2026-08-16 20:09:48 -0300

    Update embed.py

--- embed.py
+++ embed.py
@@ -1,11 +1,20 @@
 import discord
 from discord.ext import commands
 
+
 def setup_embed(bot):
 
     @bot.command(name="embed")
     @commands.has_permissions(administrator=True)
-    async def embed(ctx, canal: discord.TextChannel, cor, titulo, url, *, mensagem):
+    async def embed(
+        ctx,
+        canal: discord.TextChannel,
+        cor,
+        titulo,
+        url,
+        *,
+        mensagem
+    ):
 
         cores = {
             "vermelho": discord.Color.red(),
@@ -17,28 +26,51 @@ def setup_embed(bot):
             "branco": discord.Color.light_grey()
         }
 
+        cor_escolhida = cores.get(
+            cor.lower(),
+            discord.Color.red()
+        )
+
         emb = discord.Embed(
             title=titulo,
             description=mensagem,
-            color=cores.get(cor.lower(), discord.Color.red())
+            color=cor_escolhida
         )
 
-        emb.set_author(
-            name="FAL Community",
-            icon_url=ctx.guild.icon.url if ctx.guild.icon else None
+        if ctx.guild.icon:
+
+            emb.set_author(
+                name="FAL Community",
+                icon_url=ctx.guild.icon.url
+            )
+
+            emb.set_thumbnail(
+                url=ctx.guild.icon.url
+            )
+
+        if url.lower() != "none":
+
+            emb.set_image(
+                url=url
+            )
+
+        if ctx.author.avatar:
+
+            emb.set_footer(
+                text=f"Enviado por {ctx.author}",
+                icon_url=ctx.author.avatar.url
+            )
+
+        else:
+
+            emb.set_footer(
+                text=f"Enviado por {ctx.author}"
+            )
+
+        await canal.send(
+            embed=emb
         )
 
-        emb.set_thumbnail(
-            url=ctx.guild.icon.url if ctx.guild.icon else None
+        await ctx.message.add_reaction(
+            "✅"
         )
-
-        emb.set_footer(
-            text=f"Enviado por {ctx.author}",
-            icon_url=ctx.author.avatar.url if ctx.author.avatar else None
-        )
-
-        emb.set_image(url=url)
-
-        await canal.send(embed=emb)
-
-        await ctx.message.add_reaction("✅")
